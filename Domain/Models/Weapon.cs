5e133684af4bcbb7e667c76747e1534536c6a41c
kocipazur <naturat.am@gmail.com>
2021-05-23 22:48:09 +0200

    [Alpha] Added EFCore

---
+++ Domain/Models/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Actors.Models
+{
+    public class Weapon
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal BaseDamage { get; set; }
+
+    }
+}
---
+++ Infrastructure/Persistence/GameContext.cs
@@ -0,0 +1,15 @@
+﻿using Actors.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class GameDbContext : DbContext
+    {
+        public DbSet<Weapon> Weapons { get; set; }
+        public GameDbContext(DbContextOptions<GameDbContext> options)
+                    : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
